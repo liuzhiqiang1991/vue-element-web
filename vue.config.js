222121506e04bb79c90eb207c52ae63bbac820fd
liuzhiqiang <liuzhiqiang199101@163.com>
2021-06-20 22:49:31 +0800

    目录改为code

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/main/',    //这里是文件路径，线上必须修改(/main/ | /)
+  publicPath: '/code/',    //这里是文件路径，线上必须修改(/main/ | /)
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
